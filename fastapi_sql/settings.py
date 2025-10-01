fed45404b7056f054a7bffe1114555e89a255de8
Juan Plazas <juanplazasb84@gmail.com>
2025-10-01 10:32:56 -0500

    UPDATE | created base for a sqlite FastAPI app with migrations and tortoise

---
+++ fastapi_sql/settings.py
@@ -0,0 +1,1 @@
+TORTOISE_ORM={'connections': {'models': {'engine': 'tortoise.backends.sqlite', 'credentials': {'journal_mode': 'WAL', 'journal_size_limit': 16384, 'file_path': 'data/alex.db'}}}, 'apps': {'models': {'models': ['models', "aerich.models"], 'default_connection': 'models'}}}
