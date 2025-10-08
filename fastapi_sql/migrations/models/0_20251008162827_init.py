ca90c2225fd482e172d770a227f74778e39acb56
Juan Plazas <juanplazasb84@gmail.com>
2025-10-08 17:22:00 -0500

    c

--- fastapi_sql/migrations/models/0_20251008162827_init.py
+++ fastapi_sql/migrations/models/0_20251008162827_init.py
@@ -3,8 +3,16 @@ from tortoise import BaseDBAsyncClient
 
 async def upgrade(db: BaseDBAsyncClient) -> str:
     return """
-        CREATE TABLE IF NOT EXISTS "users" (
-    "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        CREATE TABLE IF NOT EXISTS "documents" (
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "modified_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "id" CHAR(36) NOT NULL PRIMARY KEY,
+    "content" TEXT NOT NULL
+);
+CREATE TABLE IF NOT EXISTS "users" (
+    "created_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "modified_at" TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    "id" CHAR(36) NOT NULL PRIMARY KEY,
     "username" VARCHAR(50) NOT NULL UNIQUE,
     "email" VARCHAR(100) NOT NULL UNIQUE,
     "password_hash" VARCHAR(255) NOT NULL
--- gemini_model/genai_handler.py
+++ gemini_model/genai_handler.py
@@ -2,6 +2,14 @@ import google.generativeai as genai
 from settings import API_KEY
 
 
+PLANTILLA = """"
+usted es un contador experto en leyes colombianas basadas en la informacion de 2025.
+responda a este mensaje 
+{mensaje}
+pongale un resumen al final de todo lo que se encontró
+    """
+
+
 def call_genai(message):
     genai.configure(api_key=API_KEY)
 
@@ -10,8 +18,10 @@ def call_genai(message):
 
     # Start a chat session
     chat = model.start_chat(history=[])
+    # chat = model.get_chat(id="")
 
     # Send a message and print the response
-    response = chat.send_message(message)
+    full_message = PLANTILLA.format(message)
+    response = chat.send_message(full_message)
 
     return response.text
